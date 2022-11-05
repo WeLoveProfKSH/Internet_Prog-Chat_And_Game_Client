15365de1f5c7cc324af4a3a0cdff2aafbb590eed
VM <VM@DESKTOP-LS404L8>
2022-11-05 22:53:45 +0900

    Client 기본 설계 완료

--- TCPClient.cpp
+++ TCPClient.cpp
@@ -1,23 +1,54 @@
-#include "..\Common.h"
+#include "Common.h"
+#include <iostream>
+#include <string>
 
 char *SERVERIP = (char *)"127.0.0.1";
 #define SERVERPORT 9000
 #define BUFSIZE    512
 
-int main(int argc, char *argv[])
-{
-	int retval;
+// 데이터 통신에 사용할 변수
+SOCKET sock;
+char buf[BUFSIZE + 1];
+char input[BUFSIZE + 1];
+int len;
+int retval;
 
+std::string nick; // 채팅방 닉네임
+
+DWORD WINAPI Svrrecv(LPVOID arg){	// 메시지 수신 스레드용
+	char r[BUFSIZE + 1];
+
+	while (1) {
+		// 데이터 받기
+		strcpy(r, "");
+		retval = recv(sock, r, BUFSIZE, 0);
+
+		// 받은 데이터 출력
+		r[retval] = '\0';
+		//printf("[TCP 클라이언트] %d바이트를 받았습니다.\n", retval);
+		printf("[메시지 수신] >> %s\n\n", r);
+	}
+}
+
+int main(int argc, char *argv[]){
 	// 명령행 인수가 있으면 IP 주소로 사용
 	if (argc > 1) SERVERIP = argv[1];
+	if (argc > 2) nick = argv[2];
+
+	if (nick == "") {
+		printf("채팅방에서 사용할 이름을 정하십시오 : ");
+		std::cin >> nick;
+		getchar(); // 입력버퍼 비우기
+	}
+	nick = nick + " : ";
 
 	// 윈속 초기화
-	WSADATA wsa;
+	WSADATA wsa{};
 	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
 		return 1;
 
 	// 소켓 생성
-	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+	sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (sock == INVALID_SOCKET) err_quit("socket()");
 
 	// connect()
@@ -29,22 +60,37 @@ int main(int argc, char *argv[])
 	retval = connect(sock, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
 	if (retval == SOCKET_ERROR) err_quit("connect()");
 
-	// 데이터 통신에 사용할 변수
-	char buf[BUFSIZE + 1];
-	int len;
+	// 서버로부터 데이터 수신 시작 (수신 전용 스레드 하나 돌리기)
+	HANDLE hThread = CreateThread(NULL, 0, Svrrecv, (LPVOID)sock, 0, NULL);
+	if (hThread == NULL) { closesocket(sock); }
+	else { CloseHandle(hThread); }
 
+	printf("<글자를 입력하시고 엔터를 누르시면 메시지가 전송됩니다.>\n");
+	for(int i = 0; i < 30; i++)	printf("──");
+	printf("\n\n");
 	// 서버와 데이터 통신
 	while (1) {
+		//printf("\nbuf : '%s', input : '%s'\n", buf, input);
+		strcpy(buf, nick.c_str());
+
 		// 데이터 입력
-		printf("\n[보낼 데이터] ");
-		if (fgets(buf, BUFSIZE + 1, stdin) == NULL)
+		//printf("\n입력 : ");
+		if (fgets(input, BUFSIZE + 1, stdin) == NULL)
 			break;
 
-		// '\n' 문자 제거
+		strncat(buf, input, sizeof(input));
+
+		// '\n' 문자 제거1
 		len = (int)strlen(buf);
 		if (buf[len - 1] == '\n')
 			buf[len - 1] = '\0';
 		if (strlen(buf) == 0)
+			break;
+		// '\n' 문자 제거2
+		len = (int)strlen(input);
+		if (input[len - 1] == '\n')
+			input[len - 1] = '\0';
+		if (strlen(input) == 0)
 			break;
 
 		// 데이터 보내기
@@ -53,21 +99,7 @@ int main(int argc, char *argv[])
 			err_display("send()");
 			break;
 		}
-		printf("[TCP 클라이언트] %d바이트를 보냈습니다.\n", retval);
-
-		// 데이터 받기
-		retval = recv(sock, buf, retval, MSG_WAITALL);
-		if (retval == SOCKET_ERROR) {
-			err_display("recv()");
-			break;
-		}
-		else if (retval == 0)
-			break;
-
-		// 받은 데이터 출력
-		buf[retval] = '\0';
-		printf("[TCP 클라이언트] %d바이트를 받았습니다.\n", retval);
-		printf("[받은 데이터] %s\n", buf);
+		printf("[TCP 클라이언트] %d바이트를 보냈습니다.\n\n", retval);
 	}
 
 	// 소켓 닫기
