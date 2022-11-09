435c541e84c4ee245380eb11470d6e14cfe72ed6
meatspinboy <meatspinboy@DESKTOP-5K5LN1K>
2022-11-09 11:00:43 +0900

    Client UI 개선

--- TCPClient.cpp
+++ TCPClient.cpp
@@ -2,7 +2,9 @@
 #include <direct.h>
 #include <iostream>
 #include <string>
+#include <windows.h>
 
+HANDLE consoleHandler = GetStdHandle(STD_OUTPUT_HANDLE);
 char* SERVERIP = (char*)"127.0.0.1";
 #define SERVERPORT 9000
 #define BUFSIZE    512
@@ -16,6 +18,25 @@ int retval;
 
 std::string nick; // 채팅방 닉네임
 
+int wherey()	//	Console 창에서의 커서 y값 반환해주는 메서드
+{
+	if (consoleHandler == INVALID_HANDLE_VALUE)
+		return 0;
+	CONSOLE_SCREEN_BUFFER_INFO screenInfo;
+	GetConsoleScreenBufferInfo(consoleHandler, &screenInfo);
+	return screenInfo.dwCursorPosition.Y + 1;
+}
+
+
+int gotoxy(int x, int y) // Console창 커서 위치 이동
+{
+	if (consoleHandler == INVALID_HANDLE_VALUE)
+		return 0;
+	COORD coords = { static_cast<short>(x), static_cast<short>(y)-1 };
+	SetConsoleCursorPosition(consoleHandler, coords);
+	return 1;
+}
+
 DWORD WINAPI Svrrecv(LPVOID arg) {	// 메시지 수신 스레드용
 	char r[BUFSIZE + 1];	// 수신한 데이터를 담아둘 char 배열
 
@@ -26,7 +47,9 @@ DWORD WINAPI Svrrecv(LPVOID arg) {	// 메시지 수신 스레드용
 		// 받은 데이터 출력
 		r[retval] = '\0';
 		//printf("[TCP 클라이언트] %d바이트를 받았습니다.\n", retval);
+		gotoxy(0, wherey());
 		printf("[메시지 수신] >> %s\n\n", r);
+		printf("송신할 메시지 입력 >> ");
 	}
 }
 
@@ -77,6 +100,7 @@ int main(int argc, char* argv[]) {
 		strcpy(buf, nick.c_str());	// 일단 buf에 "닉네임 : " 이런식으로 저장하기
 
 		// 보낼 데이터 입력
+		printf("송신할 메시지 입력 >> ");
 		if (fgets(input, BUFSIZE + 1, stdin) == NULL)	// fgets()함수로 input에 입력받는데, 아무것도 입력하지 않으면 break;
 			break;
 
@@ -122,3 +146,9 @@ int main(int argc, char* argv[]) {
 	WSACleanup();
 	return 0;
 }
+
+
+/*
+참고문헌 목록
+https://pracon.tistory.com/123 (cmd 콘솔에서의 gotoxy()함수를 이용한 커서 위치 이동 테크닉)
+*/
